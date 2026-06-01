b36d6847a406cd673135044f1a001acc16a21d12
p4ulbr4dl3y <chirkovd02@gmail.com>
2026-06-01 06:53:08 +0300

    fix: restrict uvicorn watch to app dir

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 import uvicorn
 
 def main():
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True, reload_dirs=["app"])
 
 if __name__ == "__main__":
     main()
